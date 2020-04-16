5347c5731010956cad71c293ee28efd54c5c6e54
anirudh_r_hub <anirudhramani16@gmail.com>
2020-04-16 18:15:27 +0530

    added text to speech and speech recognition

--- app/src/main/java/com/example/pdfreader/MainActivity.java
+++ app/src/main/java/com/example/pdfreader/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.pdfreader;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,12 +10,22 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.List;
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextToSpeech text_to_speech;
+    private SpeechRecognizer speechRecognizer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,31 +37,123 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,1);
+                speechRecognizer.startListening(intent);
+            }
+
+        });
+        initialise_text_to_speech();
+        initialise_speech_recognizer();
+    }
+
+    private void initialise_text_to_speech() {
+        text_to_speech=new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(text_to_speech.getEngines().size()==0)
+                {
+                    Toast.makeText(MainActivity.this,"There is no speech engine",
+                            Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                else
+                {
+                    text_to_speech.setLanguage(Locale.US);
+
+                }
+
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private void initialise_speech_recognizer() {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        if(SpeechRecognizer.isRecognitionAvailable(this))
+        {
+            speechRecognizer=SpeechRecognizer.createSpeechRecognizer(this);
+            speechRecognizer.setRecognitionListener(new RecognitionListener() {
+                @Override
+                public void onReadyForSpeech(Bundle params) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                }
+
+                @Override
+                public void onBeginningOfSpeech() {
+
+                }
+
+                @Override
+                public void onRmsChanged(float rmsdB) {
+
+                }
+
+                @Override
+                public void onBufferReceived(byte[] buffer) {
+
+                }
+
+                @Override
+                public void onEndOfSpeech() {
+
+                }
+
+                @Override
+                public void onError(int error) {
+
+                }
+
+                @Override
+                public void onResults(Bundle results)
+                {
+                    List<String> speech_results=results.getStringArrayList(
+                            SpeechRecognizer.RESULTS_RECOGNITION
+                    );
+                    process_result(speech_results.get(0));
+
+                }
+
+                @Override
+                public void onPartialResults(Bundle partialResults) {
+
+                }
+
+                @Override
+                public void onEvent(int eventType, Bundle params) {
+
+                }
+            });
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    private void process_result(String command) {
+        command=command.toLowerCase();
+
+        if(command.equals("hello"))
+        {
+            speak("bye");
+
+        }
+        else if(command.equals("bye"))
+        {
+            speak("hello");
+        }
+        else
+            speak("Sorry cannot understand");
+
+    }
+
+    private void speak(String message)
+    {
+        if(Build.VERSION.SDK_INT>=21)
+        {
+            text_to_speech.speak(message,TextToSpeech.QUEUE_ADD,null,null);
+        }
+    }
+
+
+
+
 }
